81ba12c4394bf5866d3859cec8d4ca3e4fa9ff9d
marlon-espinoza <marlon_choko@hotmail.com>
2015-02-04 05:17:03 -0800

    timepidecker agregado

--- app/src/main/java/com/example/jegerima/SIDWeb/PersonalTask.java
+++ app/src/main/java/com/example/jegerima/SIDWeb/PersonalTask.java
@@ -1,45 +1,50 @@
 package com.example.jegerima.SIDWeb;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
-import android.support.v7.app.ActionBarActivity;
 
 import com.example.jegerima.SIDWeb.Objetos.Lista;
 import com.example.jegerima.SIDWeb.database.DataBaseManagerCourses;
 import com.example.jegerima.SIDWeb.database.DataBaseManagerNotes;
 import com.example.jegerima.SIDWeb.timedialogfragment.DatePickerFragment;
-import com.example.jegerima.SIDWeb.views.TaskView;
+import com.example.jegerima.SIDWeb.timedialogfragment.TimePickerFragment;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 
-public class PersonalTask extends ActionBarActivity {
+public class PersonalTask extends ActionBarActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private DataBaseManagerNotes dbApuntes;
     private Cursor cursor;
 
     private EditText titulo;
     private Spinner materia;
     private EditText fecha_venc_apunte;
+    private EditText hora_venc_apunte;
+    //Para obtener fecha actual
+
     private EditText apuntes;
     //Para obtener fecha actual
     ContentValues valores = new ContentValues();
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
     Date date = new Date();
 
     @Override
@@ -87,7 +92,19 @@ public class PersonalTask extends ActionBarActivity {
             }
         });
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        hora_venc_apunte.setText(timeFormat.format(date));
+        hora_venc_apunte.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    // Always use a TextKeyListener when clearing a TextView to prevent android
+                    // warnings in the log
+                    showTimePickerDialog(v);
+
+                }
+            }
+        });
 
     }
 
@@ -142,5 +159,31 @@ public class PersonalTask extends ActionBarActivity {
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "datePicker");
+    }
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendario = Calendar.getInstance();
+        calendario.set(Calendar.YEAR, year);
+        calendario.set(Calendar.MONTH, monthOfYear);
+        calendario.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        Date d = new Date();
+        d.setTime(calendario.getTimeInMillis());
+        fecha_venc_apunte.setText(dateFormat.format(d));
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar hora = Calendar.getInstance();
+        hora.set(Calendar.HOUR,hourOfDay);
+        hora.set(Calendar.MINUTE,minute);
+        Date d = new Date();
+        d.setTime(hora.getTimeInMillis());
+        hora_venc_apunte.setText(timeFormat.format(d));
     }
 }
